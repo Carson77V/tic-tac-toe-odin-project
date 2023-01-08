0ef2850810c45ab7b0bf0709ffa62835d65cb99c
Carson <carsontwj@gmail.com>
2023-01-08 11:11:13 -0500

    Add functions to check board for winners

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //The module for the game board (tic tac toe)
 const gameBoard = (function () {
-    let _array = ["1","2","3","4","5","6","7","8","9"]
+    let _array = ["","","","","","","","",""]
 
     // called to update the array when a new move is made
     const updateArray = function (sign, index) {
@@ -25,6 +25,7 @@ const gameBoard = (function () {
 //module will be used to check inputs like a controller
 const gameController = (function () {
     let sign = 'X'
+    let xArray = [], oArray = []
     //select each space from the table, creates a nodelist
     const boardSpots = document.querySelectorAll('td')
     //loop through the nodelist and give each boardSpot an event listener
@@ -32,6 +33,7 @@ const gameController = (function () {
         ele.addEventListener('click', () => {
             //check for availability and add player sign
             _spotAvailable(ele, gameBoard.getArray(), ele.id, sign)
+            _checkWinner()
         })
     })
 
@@ -42,14 +44,75 @@ const gameController = (function () {
             //render the sign into the board spot
             ele.textContent = sign
             //change turn after spot has been confirmed
-            _changeTurn()
+            _changeTurn(i)
         }
     }
 
     //returns the sign of the player who's turn it is
-    const _changeTurn = function() {
-        if (sign === 'X') sign = 'O'
-        else sign = 'X'
+    const _changeTurn = function(i) {
+        if (sign === 'X') {
+            xArray.push(i)
+            sign = 'O'
+        }
+        else {
+            oArray.push(i)
+            sign = 'X'
+        }
+    }
+
+    //checks the board for a winner
+    const _checkWinner = function() {
+        // let xArray = [], oArray = []
+        // const array = gameBoard.getArray()
+        // //loop through the gameBoard array
+        // for (let i = 0; i < array.length - 1; i++) {
+        //     //saves all indexes of X and O to new arrays.
+        //     //used later to compare against winning patterns
+        //     if (array[i] === 'X') xArray.push(i)
+        //     else if (array[i] === 'O') oArray.push(i)
+        // }
+
+        if (_matchPatterns(xArray)) console.log('winner')
+        if (_matchPatterns(oArray)) console.log('winner')
+
+    }
+
+    const _matchPatterns = function(array) {
+        const winners = _winPatterns()
+        //when match = 3, it has three matches and the player wins
+        let match = 0
+        //first two loops are for the 2D winner[][] array
+        for (let i = 0; i < winners.length; i++) {
+            for (let j = 0; j < 3; j++) {
+                //this loop is for the array passed into the function
+                // It will check if the array has the three numbers that match
+                //the winning pattern
+                // if the array has a match, increment match
+                for (let k = 0; k < array.length; k++) {
+                    if (array[k] == (winners[i][j])) match++
+                }
+            }
+            //if 3 numbers match there is a winner. End the function
+            if (match === 3) {
+                return true
+            }
+            //if there aren't 3 matches reset the match count. Try again
+            match = 0
+        }
+    }
+
+    //these are all the patterns of array indexes required to win
+    const _winPatterns = function() {
+        return [
+            [0, 1, 2],
+            [0, 3, 6],
+            [0, 4, 8],
+            [1, 4, 7],
+            [2, 5, 8],
+            [2, 4, 6],
+            [3, 4, 5],
+            [6, 7, 8]
+        ]
     }
 
 })()
@@ -58,8 +121,6 @@ const gameController = (function () {
 const Player = function (name, sign, turn) {
     this.name = name
     this.sign = sign
-    //true if it's this players turn
-    this.turn = turn
 
     //returns the players sign
     const getSign = function() {
@@ -69,5 +130,5 @@ const Player = function (name, sign, turn) {
     return {getSign}
 }
 
-let player1 = Player('player1', 'X', true)
-let player2 = Player('player2', 'O', false)
+let player1 = Player('player1', 'X')
+let player2 = Player('player2', 'O')
